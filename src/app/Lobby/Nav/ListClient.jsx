d07170ca8b25b0537c08343feddfee8e7a8c072a
Daniel 'Oficina <danielfloresd92@gmail.com>
2025-07-22 13:41:03 -0400

    eliminación de fetching de datos en servidor

--- src/app/Lobby/Nav/ListClient.jsx
+++ src/app/Lobby/Nav/ListClient.jsx
@@ -1,20 +1,23 @@
+'use client';
 import List from './List.jsx';
-import getListClient from '@/libs/ajaxServer/getListClient';
+import { useSingleFetch } from '@/hook/ajax_hook/useFetch'
 
 
-export default async function ListClient(){
 
-    const listClient = await getListClient(false);
+
+export default function ListClient() {
+
+    const { data, error } = useSingleFetch({ resource: '/localLigth', method: 'get' }, true);
 
     return (
         <>
             <ul className='listRoute-a-menuTitle scrolltheme1 flex columns'>
                 {
-                    Array.isArray(listClient) ?
-                        listClient.map((item, index) => (
-                            <List data={item} key={index} />
+                    Array.isArray(data) ?
+                        data.map((item) => (
+                            <List data={item} key={item._id} />
                         ))
-                    :
+                        :
                         null
                 }
             </ul>
